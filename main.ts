3014e9e97efd7a6e05b356474aec65ef27370566
attys79 <75606949+attys79@users.noreply.github.com>
2020-12-09 11:06:57 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    if (count > 0) {
+        count += -1
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (count < 9) {
+        count += 1
+    }
+})
+let count = 0
+count = 0
 basic.forever(function () {
-	
+    basic.showNumber(count)
 })
