29eafec44652ab3d7f0d11e376212ed458f7870c
attys79 <75606949+attys79@users.noreply.github.com>
2020-12-09 11:35:18 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,27 @@
+let time = 0
+let stopwatch = ""
+input.onButtonPressed(Button.A, function () {
+    time = input.runningTime()
+    stopwatch = "start"
+    led.stopAnimation()
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    time = 0
+    led.stopAnimation()
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    time = input.runningTime() - time
+    stopwatch = "stop"
+    led.stopAnimation()
+    basic.clearScreen()
+})
 basic.forever(function () {
-	
+    if (stopwatch == "start") {
+        led.toggle(2, 2)
+        basic.pause(100)
+    } else {
+        basic.showNumber(time)
+    }
 })
