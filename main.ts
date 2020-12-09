8de7ab8d69e799a757f260f2ec240b0266cf6370
attys79 <75606949+attys79@users.noreply.github.com>
2020-12-09 11:13:20 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
 basic.forever(function () {
-	
+    for (let i = 0; i <= 9; i++) {
+        basic.showNumber(i)
+        basic.pause(1000)
+    }
+    for (let i = 0; i <= 7; i++) {
+        basic.showNumber(8 - i)
+        basic.pause(1000)
+    }
 })
