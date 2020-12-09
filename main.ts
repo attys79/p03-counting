c3619b82e3dce8e3964d891a97abb169efeb491f
attys79 <75606949+attys79@users.noreply.github.com>
2020-12-09 12:15:25 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,26 @@
 input.onButtonPressed(Button.A, function () {
     if (timer == "on") {
-        timer = "off"
         time = input.runningTime() - time
+        timer = "off"
         basic.clearScreen()
     }
 })
 input.onButtonPressed(Button.B, function () {
     if (timer == "on") {
-        timer = "off"
         time = input.runningTime() - time
+        timer = "off"
         basic.clearScreen()
     }
 })
-let time = 0
 let timer = ""
+let time = 0
 for (let i = 0; i <= 3; i++) {
     basic.showNumber(3 - i)
 }
 basic.clearScreen()
 basic.pause(randint(100, 5000))
-timer = "on"
 time = input.runningTime()
+timer = "on"
 basic.showIcon(IconNames.Target)
 basic.forever(function () {
     if (timer == "off") {
